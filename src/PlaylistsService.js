daad8c970a1de657419519f71491c09233ac18d2
Wildan <akudia832@gmail.com>
2023-08-22 18:02:37 +0800

    consumer implemented

---
+++ src/PlaylistsService.js
@@ -0,0 +1,41 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    // eslint-disable-next-line no-underscore-dangle
+    this._pool = new Pool();
+  }
+
+  async getSongsFromPlaylist(playlistId) {
+    const QueryPlaylist = {
+      text: `SELECT playlists.id as "id", playlists.name as "name", users.username as "username" 
+      FROM playlists_song
+      INNER JOIN playlists ON playlists_song.playlist_id = playlists.id 
+      INNER JOIN users ON playlists.owner = users.id
+      WHERE playlists.id = $1`,
+      values: [playlistId],
+    };
+
+    const QuerySong = {
+      text: `SELECT songs.id, songs.title, songs.performer
+      FROM songs
+      INNER JOIN playlists_song ON songs.id = playlists_song.song_id
+      WHERE playlists_song.playlist_id = $1`,
+      values: [playlistId],
+    };
+
+    // eslint-disable-next-line no-underscore-dangle
+    const playlistResult = await this._pool.query(QueryPlaylist);
+    // eslint-disable-next-line no-underscore-dangle
+    const songResult = await this._pool.query(QuerySong);
+
+    return {
+      id: playlistResult.rows[0].id,
+      name: playlistResult.rows[0].name,
+      username: playlistResult.rows[0].username,
+      songs: songResult.rows,
+    };
+  }
+}
+
+module.exports = PlaylistsService;
---
+++ src/listener.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+/* eslint-disable no-underscore-dangle */
+class Listener {
+  constructor(playlistsService, mailSender) {
+    this._playlistsService = playlistsService;
+    this._mailSender = mailSender;
+
+    this.listen = this.listen.bind(this);
+  }
+
+  async listen(message) {
+    try {
+      const { userId, targetEmail } = JSON.parse(message.content.toString());
+
+      const playlistsongs = await this._playlistsService.getSongsFromPlaylist(userId);
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlistsongs));
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+module.exports = Listener;
